675dbe05a492ee41d3515ae4fdef49664e7c259f
danvolsky-source <dan.volsky@gmail.com>
2025-12-16 20:53:07 +0200

    Create sentry.server.config.js

---
+++ sentry.server.config.js
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // Adjust this value in production
+  tracesSampleRate: 1.0,
+  
+  debug: false,
+  
+  environment: process.env.NODE_ENV,
+});
